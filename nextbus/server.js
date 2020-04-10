8641571f794b78d1cd49ff1e696d37bee6dad61f
Hugh Brown <hugh@balena.io>
2020-04-09 17:11:15 -0700

    Switch from requests to got
    Signed-off-by: Hugh Brown <hugh@balena.io>

--- nextbus/server.js
+++ nextbus/server.js
@@ -1,33 +1,46 @@
 const http = require('http');
-const request = require('requests');
+const got = require('got');
 
 const hostname = '0.0.0.0';
 const stop = process.env.BUSSTOP;
 const port = 3000;
 const apikey = process.env.APIKEY;
-const translinkUrl = 'https://api.translink.ca/RTTIAPI/V1/stops/53620/estimates?apiKey=$APIKEY Accept:application/JSON'
 
 function buildTranslinkQuery() {
-    const query = 'https://api.translink.ca/RTTIAPI/V1/stops/' + stop + '/estimates?apiKey' + apikey + ' Accept: application/JSON';
-    return query;
+  const query = 'https://api.translink.ca/RTTIAPI/V1/stops/' + stop + '/estimates?apiKey=' + apikey
+  return query;
 
 }
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    query = buildTranslinkQuery();
-    request(query, { 'json': 'true' }, (err, res, body) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        }
-        console.log(body.url);
-        res.end(body.url);
-        console.log(body.explanation);
+console.log('Hello, world!');
+
+(async () => {
+  try {
+    req = buildTranslinkQuery()
+    console.log(req);
+    response = await got(req, {
+      headers: {
+	'content-type': 'application/json'
+      }
     });
+    data = JSON.parse(response.body);
+    console.log(response.body);
+    console.log(data);
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  // res.write('Hello, world!');
+  res.write(data);
+  res.end();
 });
 
+
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
